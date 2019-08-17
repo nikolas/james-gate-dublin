047b751c436c29bea189f3cc485efdae71ac84e7
Nikolas Nyby <nikolas@gnu.org>
2019-08-16 22:20:05 -0400

    "It's getting there"

--- hello.py
+++ hello.py
@@ -1,40 +1,8 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return """
-<!doctype html>
-<html>
-<head>
-<title>Lennart Poettering's cool shop!</title>
-<style type="text/css">
-body {
-    background: purple;
-    color: white;
-    font-size: 22px;
-}
-p {
-    margin-top: 2em;
-    transform: rotate(3deg);
-}
-</style>
-</head>
-<body>
-<h1>Lennart Poettering's cool shop!</h1>
-
-<h3>aka Nik &amp; Pria's t-shirt business / cloud.</h3>
-<p>
-Lennart Poettering's cool shop!
-Lennart Poettering's cool shop!
-Lennart Poettering's cool shop!Lennart Poettering's cool shop!
-Lennart Poettering's cool shop!Lennart Poettering's cool shop!
-Lennart Poettering's cool shop!Lennart Poettering's cool shop!
-Lennart Poettering's cool shop!Lennart Poettering's cool shop!
-Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!Lennart Poettering's cool shop!
-</p>
-
-</body>
-</html>
-"""
+    return render_template('hello.html')
